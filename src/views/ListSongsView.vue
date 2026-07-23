d5a10ceaa5d5d38a4e0a5f01dfda751e02923b5d
Bernardo Gracioso <bernardo.gracioso@gmail.com>
2026-07-24 00:05:40 +0100

    order musics in ListSongsView.vue

--- src/views/ListSongsView.vue
+++ src/views/ListSongsView.vue
@@ -4,9 +4,58 @@ import Cart from '@/components/Cart.vue';
 
 import songs_map from '@/songs_map.json';
 import { ref } from 'vue';
+import { computed } from 'vue';
 
 const songs = ref(songs_map);
-console.log(songs.value);
+
+const sortColumn = ref(null);
+const sortOrder = ref('asc');
+
+function sortBy(column) {
+  if (sortColumn.value === column) {
+    // clicking the same column again flips the order....
+    sortOrder.value = sortOrder.value === 'asc' ? 'desc' : 'asc';
+
+  } else {
+    sortColumn.value = column;
+    sortOrder.value = 'asc';
+  }
+}
+
+const sortedSongs = computed(() => {
+  const entries = Object.entries(songs.value);
+
+  if (!sortColumn.value) return entries;
+
+  const col = sortColumn.value;
+  const dir = sortOrder.value === 'asc' ? 1 : -1;
+
+  return [...entries].sort(([, a], [, b]) => {
+    const valA = a[col];
+    const valB = b[col];
+
+    if (valA == null) return 1;
+    if (valB == null) return -1;
+
+    if (typeof valA === 'string') {
+      return valA.localeCompare(valB) * dir;
+    }
+    return (valA - valB) * dir;
+  });
+});
+
+const toggle = (sortColumn, desiredColumn, sortOrder) => {
+    if (sortColumn === desiredColumn) {
+        if (sortOrder === 'asc') {
+            return '▼';
+        }
+
+        return '▲';
+    }
+
+    return '▼';
+}
+
 
 </script>
 
@@ -17,15 +66,24 @@ console.log(songs.value);
         <table class="songs-table">
             <thead>
                 <tr>
-                <th data-column="title" data-order="asc">Título &#9660</th>
-                <th data-column="moment" data-order="asc">Momento &#9660</th>
-                <th data-column="tone" data-order="asc">Tom &#9660</th>
-                <th data-column="add" data-order="asc">Adicionada &#9660</th>
+                <th @click="sortBy('title')">
+                    Título
+                    <span class="order">{{ toggle(sortColumn, 'title', sortOrder) }}</span>
+                </th>
+                <th @click="sortBy('moment')">
+                    Momento
+                    <span class="order">{{ toggle(sortColumn, 'moment', sortOrder) }}</span>
+                </th>
+                <th @click="sortBy('tone')">
+                    Tom
+                    <span class="order">{{ toggle(sortColumn, 'tone', sortOrder) }}</span>
+                </th>
+                <th>Adicionada</th>
                 </tr>
             </thead>
             <tbody id="songs-list">
                 <SongListing
-                    v-for="(song, id) in songs"
+                    v-for="[id, song] in sortedSongs"
                     :key="id"
                     :song="song"
                     :id="id"
@@ -37,3 +95,10 @@ console.log(songs.value);
     <Cart />
 
 </template>
+
+<style scoped>
+.order {
+    color: white;
+    font-weight: bold;
+}
+</style>
