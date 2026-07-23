68b763d53511f57f3414b7cd5a4beb83783a8e39
Bernardo Gracioso <bernardo.gracioso@gmail.com>
2026-07-23 16:21:09 +0100

    Vue) first home view done

---
+++ src/components/Navbar.vue
@@ -0,0 +1,22 @@
+<script setup>
+import { defineProps } from 'vue';
+
+defineProps({
+    title: {
+        type: String,
+        default: "Cancioneiro"
+    }
+});
+</script>
+
+<template>
+  <header class="main-header">
+    <nav class="nav">
+      <ul>
+        <li><a href="/index.html">INÍCIO</a></li>
+        <li><a href="/list_songs.html">LISTA DE CÂNTICOS</a></li>
+      </ul>
+    </nav>
+    <h1 class="songbook-name">{{ title }}</h1>
+  </header>
+</template>
---
+++ src/views/HomeView.vue
@@ -0,0 +1,15 @@
+<script setup>
+</script>
+
+<template>
+    <section class="content-section">
+    <h2 class="section-header">INÍCIO</h2>
+    <img class="beny-guitar-image" src="@/assets/img/beny.jpg" alt="beny-guitar">
+    <p>Este é o cancioneiro utilizado por Bernardo Gracioso para a animação das eucaristias</p>
+    <p>Para ver a lista de cânticos, clique no link acima.</p>
+    <p>Para sugestões ou correções, por favor contacte-me através do email: <a
+        href="mailto:bernardo.gracioso@gmail.com">bernardo.gracioso@gmail.com</a></p>
+    <p>Este cancioneiro é uma obra em progresso, e será atualizado com novos cânticos regularmente.</p>
+    <p>Obrigado por visitar!</p>
+  </section>
+</template>
