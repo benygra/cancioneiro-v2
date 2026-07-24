c2ff766e3b79d2ce50260217b6e64a6635eb4fd6
Bernardo Gracioso <bernardo.gracioso@gmail.com>
2026-07-24 18:57:06 +0100

    senhor_tem_piedade done!

--- src/data/lyrics/santo_e_o_senhor_meu_deus.vue
+++ src/data/lyrics/santo_e_o_senhor_meu_deus.vue
@@ -5,8 +5,8 @@ Santo é o Senhor meu Deus, digno de louvor<span class="chord">
    F#m       E        D               C#m</span>
 A Ele o poder, a honra e a glória <i>(2x)</i>
 <strong><span class="chord">
-     F#m                              E                                    D                   C#m</span>
-Hossana (hossana)! Hossana (hossana)! Hossana nas alturas <i>(2x)</i></strong>
+     F#m                              E                                   D                    C#m</span>
+Hossana (hossana)! Hossana (hossana)! Hossana oh Senhor! <i>(2x)</i></strong>
 <span class="chord">
        F#m           E             D                 C#m</span>
 Bendito o que vem em nome do Senhor,<span class="chord">
---
+++ src/data/lyrics/senhor_tem_piedade.vue
@@ -0,0 +1,30 @@
+<template>
+        <pre><span class="chord">
+C  G                     Am</span>
+Senhor, tem piedade<span class="chord">
+F  Am                  G     G7</span>
+Senhor, tem piedade<span class="chord">
+    Em                   Am</span>
+Senhor, tem piedade de nós<span class="chord">
+    F                      C</span>
+Senhor, tem piedade.
+<span class="chord">
+C    G                 Am</span>
+Cristo, tem piedade<span class="chord">
+F    Am              G     G7</span>
+Cristo, tem piedade<span class="chord">
+              Em                    Am</span>
+Oh, oh, Cristo, tem piedade de nós<span class="chord">
+              F                        C</span>
+Oh, oh, Cristo, tem piedade.
+<span class="chord">
+C  G                     Am</span>
+Senhor, tem piedade<span class="chord">
+F  Am                  G     G7</span>
+Senhor, tem piedade<span class="chord">
+    Em                   Am</span>
+Senhor, tem piedade de nós<span class="chord">
+    F                      C</span>
+Senhor, tem piedade.
+      </pre>
+</template>
