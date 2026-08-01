eb905abfeb221afc7c3f003cd0d09e978eb0aa4a
Bernardo Gracioso <bernardo.gracioso@gmail.com>
2026-08-01 23:55:27 +0100

    added more songs!

---
+++ src/assets/lyrics/se_vos_amardes.vue
@@ -0,0 +1,99 @@
+<template>
+        <pre><strong><span class="chord">
+               F             Dm         C         Gm</span>
+Se vos amardes uns aos outros,<span class="chord">
+C7                                    F   F7</span>
+Deus permanece em vós.<span class="chord">
+               Bb          Gm          F         Dm</span>
+Se vos amardes uns aos outros,<span class="chord">
+Gm                 C              F</span>
+Deus permanece em vós.</strong>
+<span class="chord">
+    F                                  Gm</span>
+1. É este o Meu Mandamento:<span class="chord">
+Am                                     C</span>
+Amai-vos como Eu vos amei.
+<span class="chord">
+    F                                          Gm</span>
+2. Não há maior prova de amor<span class="chord">
+Am                                      C</span>
+do que dar a vida pelos amigos.
+<span class="chord">
+    F                             Gm</span>
+3. Vós sereis meus amigos,<span class="chord">
+Am                                C</span>
+se fizerdes o que vos mando.
+<strong><span class="chord">
+               F             Dm         C         Gm</span>
+Se vos amardes uns aos outros,<span class="chord">
+C7                                    F   F7</span>
+Deus permanece em vós.<span class="chord">
+               Bb          Gm          F         Dm</span>
+Se vos amardes uns aos outros,<span class="chord">
+Gm                 C              F</span>
+Deus permanece em vós.</strong>
+<span class="chord">
+    F                               Gm</span>
+4. Amai os vossos inimigos<span class="chord">
+Am                                    C</span>
+e orai pelos que vos perseguem.
+<span class="chord">
+    F                                                Gm</span>
+5. Não julgueis e não sereis julgados,<span class="chord">
+Am                                  C</span>
+perdoai e sereis perdoados.
+<span class="chord">
+    F                                                                       Gm</span>
+6. Nós sabemos que passámos da morte à vida,<span class="chord">
+Am                                              C</span>
+porque nos amamos como irmãos.
+<strong><span class="chord">
+               F             Dm         C         Gm</span>
+Se vos amardes uns aos outros,<span class="chord">
+C7                                    F   F7</span>
+Deus permanece em vós.<span class="chord">
+               Bb          Gm          F         Dm</span>
+Se vos amardes uns aos outros,<span class="chord">
+Gm                 C              F</span>
+Deus permanece em vós.</strong>
+<span class="chord">
+    F                                                      Gm</span>
+7. Já não vos chamo servos, mas amigos,<span class="chord">
+Am                                                                        C</span>
+porque vos ensinei tudo o que ouvi de meu Pai.
+<span class="chord">
+    F                                                                               Gm</span>
+8. Vede como é grande o amor de Deus para connosco:<span class="chord">
+Am                                                     C</span>
+chamamo-nos e somos filhos de Deus.
+<span class="chord">
+    F                                         Gm</span>
+9. Vinde, benditos de meu Pai,<span class="chord">
+Am                                       C</span>
+entrai na alegria do meu reino.
+<strong><span class="chord">
+               F             Dm         C         Gm</span>
+Se vos amardes uns aos outros,<span class="chord">
+C7                                    F   F7</span>
+Deus permanece em vós.<span class="chord">
+               Bb          Gm          F         Dm</span>
+Se vos amardes uns aos outros,<span class="chord">
+Gm                 C              F</span>
+Deus permanece em vós.</strong>
+<span class="chord">
+      F                                                 Gm</span>
+10. Tive fome e destes-Me de comer,<span class="chord">
+Am                                           C</span>
+tive sede e destes-Me de beber.
+<span class="chord">
+      F                                                Gm</span>
+11. Era peregrino e vós Me recolhestes,<span class="chord">
+Am                                                         C</span>
+não tinha roupa e destes-Me de vestir.
+<span class="chord">
+      F                                 Gm</span>
+12. Estava doente e visitastes-Me,<span class="chord">
+Am                                C</span>
+estava preso e fostes ver-Me.
+      </pre>
+</template>
---
+++ src/assets/lyrics/tu_que_nas_margens_do_lago.vue
@@ -0,0 +1,49 @@
+<template>
+        <pre><span class="chord">
+C  G7                                   C    C7</span>
+Tu <span class="chord chord-space">  </span>que nas margens do lago,<span class="chord">
+               F      Dm                            G     G7</span>
+não buscaste <span class="chord chord-space">   </span>nem sábios nem ricos,<span class="chord">
+                   C      G7                     C      C7</span>
+mas só quiseste <span class="chord chord-space"> </span>que eu Te seguisse.
+<strong><span class="chord">
+    F       G                               C      Am</span>
+Senhor, Tu fixaste meus olhos,<span class="chord">
+          Dm      G                         C     C7</span>
+ternamente, meu nome disseste;<span class="chord">
+            F     G                              C      Am</span>
+nesse lago eu deixei minha barca<span class="chord">
+               Dm G                              C        G</span>
+pois em Ti <span class="chord chord-space">    </span>encontrei outro mar.</strong>
+<span class="chord">
+C  G7                                   C     C7</span>
+Tu <span class="chord chord-space">  </span>sabes bem o que eu tenho<span class="chord">
+                F       Dm                           G      G7</span>
+em meu barco, <span class="chord chord-space">  </span>nem ouro, nem armas,<span class="chord">
+                      C      G7               C      C7</span>
+somente as redes <span class="chord chord-space"> </span>e meu trabalho.
+<span class="chord">
+C  G7                                C    C7</span>
+Tu <span class="chord chord-space">  </span>minhas mãos requisitas,<span class="chord">
+              F       Dm                             G     G7</span>
+meu trabalho, <span class="chord chord-space">  </span>que a outros descanse,<span class="chord">
+                   C     G7                           C      C7</span>
+minha amizade <span class="chord chord-space"> </span>que ao mundo abrace.
+<strong><span class="chord">
+    F       G                               C      Am</span>
+Senhor, Tu fixaste meus olhos,<span class="chord">
+          Dm      G                         C     C7</span>
+ternamente, meu nome disseste;<span class="chord">
+            F     G                              C      Am</span>
+nesse lago eu deixei minha barca<span class="chord">
+               Dm G                              C        G</span>
+pois em Ti <span class="chord chord-space">    </span>encontrei outro mar.</strong>
+<span class="chord">
+C  G7                                  C     C7</span>
+Tu <span class="chord chord-space">  </span>pescador de outros lagos,<span class="chord">
+             F       Dm                            G       G7</span>
+ânsia eterna, <span class="chord chord-space">  </span>das almas que esperam,<span class="chord">
+                  C      G7                         C        C7</span>
+um bom amigo <span class="chord chord-space"> </span>que assim nos chamas.
+      </pre>
+</template>
